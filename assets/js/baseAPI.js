0cac5591855681d8a194a3f7e49ed97b801c846c
majunjiao <1012548289@qq.com>
2021-01-14 20:41:16 +0800

    完成了主页开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,21 @@
 $.ajaxPrefilter(function (options) {
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+    // 统一为有权限的借口设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ""
+        }
+    }
+    options.complete = function (res) {
+        //     // 在complate可以使用responseJSON拿到服务器相应会来的数据
+        // console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 强制清空token
+            localStorage.removeItem('token');
+            //     // 强制跳转
+            location.href = '/login.html'
+        }
+        //    
+    }
+
 })
